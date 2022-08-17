2ec4c06c727bb89a5c150cba3adece93e48e9ed8
lxq <dearbosslee@gmail.com>
2022-08-17 11:41:13 +0800

    [sql] LC1873: 计算特殊奖金

---
+++ sql/LC1873_计算特殊奖金.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+
+select employee_id, 
+    if(employee_id % 2 = 1 and name not like 'M%', salary, 0) as 'bonus'
+from Employees
+order by employee_id asc
+;
